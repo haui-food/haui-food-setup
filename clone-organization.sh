9190240b5b92b5af73bdd45c20162cafdbbf7de1
dngiang <dongocgiang.edu@gmail.com>
2024-05-27 21:42:00 +0700

    ✨ Update command clone organization

--- clone-organization.sh
+++ clone-organization.sh
@@ -5,28 +5,21 @@ SCRIPT_DIR=$(dirname "$0")
 cd "$SCRIPT_DIR/.."
 
 ORG="haui-food"
-
 PAGE=1
-PER_PAGE=100
 
-while :; do
-  response=$(curl -s "https://api.github.com/orgs/$ORG/repos?per_page=$PER_PAGE&page=$PAGE")
-
-  if [ "$(echo "$response" | wc -c)" -le 2 ]; then
-    break
-  fi
-
-  echo "$response" | grep -o '"clone_url": *"[^"]*"|"name": *"[^"]*"' | sed -n 'N;s/\n/ /;s/"name": *"\([^"]*\)".*"clone_url": *"\([^"]*\)"/\1 \2/p' | while read -r repo_name repo_url; do
-    if [ "$repo_name" = "haui-food-setup" ]; then
-      echo "Skipping repository $repo_name"
-      continue
+while : ; do
+    RESPONSE=$(curl -s "https://api.github.com/orgs/$ORG/repos?per_page=100&page=$PAGE")
+    
+    if [[ -z "$RESPONSE" || "$RESPONSE" == "[]" ]]; then
+        break
     fi
-    if [ -d "$repo_name" ]; then
-      echo "Repository $repo_name already exists, skipping..."
-    else
-      git clone "$repo_url"
-    fi
-  done
+    
+    REPOS=$(echo "$RESPONSE" | grep -o '"clone_url": "[^"]*' | cut -d'"' -f4)
+    
+    for REPO in $REPOS; do
+        echo "Cloning $REPO..."
+        git clone $REPO
+    done
 
-  PAGE=$((PAGE + 1))
+    PAGE=$((PAGE + 1))
 done
